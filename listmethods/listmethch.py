f2e1f21594a5a21ba050c33887b622cea7d55537
John Clark <John.Clark@lva.virginia.gov>
2019-10-14 14:51:41 -0400

    List challenge

---
+++ listmethods/listmethch.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+                                                                                                        
+col_dict= ["blue", "Columbus"] 
+col_dict.append(1492)
+name=input("Please type your name. ")
+col_dict.append(name)
+
+print("In " + str(col_dict[2]) + " " + col_dict[1] + " sailed the ocean " + col_dict[0] + ". " + col_dict[3] + " fell off.")
+
+
+
