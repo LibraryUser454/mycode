5f3f0a484ddfa0d84e4a5b6d708e2f6b910e8644
John Clark <John.Clark@lva.virginia.gov>
2019-10-14 11:43:22 -0400

    Adding iptaker2 challenge

--- lab_input/ip_taker02.py
+++ lab_input/ip_taker02.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 user_input = input("Please enter your IP address: ")
+user_input2 = input("Please enter your vendor name: ")
 print("You told me your IP address is: " + user_input)
-
-print("You told me your IP address is:", user_input)
+print("You told me your vendor name is " + user_input2)
 
